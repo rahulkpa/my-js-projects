80ad57cb1cce7dc74fccc02f73ec797242419ece
rhl08120 <rahul.sweden123@gmail.com>
2024-10-26 11:57:39 +0530

    added few more type of operation and datatype value

--- my-js/datatypes.js
+++ my-js/datatypes.js
@@ -5,3 +5,28 @@
 
 console.log("Rahul Kumar Pandey") 
 console.log("Age is ", 35)
+
+
+//data types
+/**
+ * 
+ * number --> 0 to 2 raise to 53
+ * bigint --> something which is bigger than number
+ * String --> usually kept in double quotes but can also be represented with Single quotes
+ * boolean --> true/false
+ * null--> it is standard value which represent empty in java script
+ * undefined --> if a variable is defined but it is unassigned
+ * symbol--> it is very much important to find unique component
+ * 
+ * Object--> 
+ * 
+ * */ 
+
+const negativeNumber = -1 
+let salary = 8928.98898
+console.log(typeof age) //since age variable is not defined it will print undefined as value
+console.log(typeof negativeNumber)
+console.log(typeof salary)
+console.log(typeof undefined) //it will print undefined as its type 
+console.log(typeof null) //it will print object 
+console.log(typeof Symbol)
