dcfab4d08d291fd205f6b82e9da2641e387645bf
rhl08120 <rahul.sweden123@gmail.com>
2024-10-26 11:32:49 +0530

    updated read me  and data types file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # my-js-projects
 Code repo for js learning
+
+# ECMA Script URL:
+
+https://tc39.es/ecma262/
+
+# Web-Api MDN Url:
+
+https://developer.mozilla.org/en-US/docs/Web/API
+
+
---
+++ my-js/datatypes.js
@@ -0,0 +1,7 @@
+"Use Strict"
+
+//alert("Rahul") // It will not work in node js and there is no HTML document involvement unlike browser
+
+
+console.log("Rahul Kumar Pandey") 
+console.log("Age is ", 35)
